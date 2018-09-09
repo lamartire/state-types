de7f1b01e6a4066e0134ebceb3534540779117ea
lamartire <lamartire@gmail.com>
2018-09-09 13:39:57 +0300

    chore(index): add some jsdoc
    Add jsdoc notation for main function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { ITypes } from './types'
 
+/**
+ * Creates re-usable types object for easy and sematic mapping unique actions types
+ * @example
+ * types('PREFIX', ['foo', 'bar'])
+ * // will return
+ * { foo: 'PREFIX/foo', bar: 'PREFIX/bar' }
+ * @param {String} prefix Types prefix
+ * @param {Array<String>} types Types list
+ * @returns {Object} Types object
+ */
 const types = (prefix: string, types: string[] = []): ITypes =>
   types.reduce(
     (acc, type) =>
