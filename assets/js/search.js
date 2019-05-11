f1a368ca1d9b2384ccececfb3cfc1f4f3e7509ac
lamartire <lamartire@gmail.com>
2019-05-11 16:46:30 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","256":"Interface"},"rows":[{"id":0,"kind":256,"name":"ITypes","url":"interfaces/itypes.html","classes":"tsd-kind-interface"},{"id":1,"kind":64,"name":"types","url":"globals.html#types","classes":"tsd-kind-function"}]};
